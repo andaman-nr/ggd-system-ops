3251d86421978989c4926d0f26f8e5dd801628e5
ANDAMAN NIRA <andaman.nr@gmail.com>
2024-02-13 08:31:59 +0700

    Update Swagger configuration

--- combination-api/apps/combination-api/mcs-fastify/src/app/plugins/swagger.ts
+++ combination-api/apps/combination-api/mcs-fastify/src/app/plugins/swagger.ts
@@ -8,33 +8,39 @@ export default fp(async function (fastify: FastifyInstance) {
   fastify.register(fastifySwagger, {
     swagger: {
       info: {
-        title: 'Test swagger',
-        description: 'Testing the Fastify swagger API',
-        version: '0.1.0',
+        title: 'Combination API',
+        description: 'This Api is a combination of multiple services into one.',
+        version: '1.0.0',
       },
-      externalDocs: {
-        url: 'https://swagger.io',
-        description: 'Find more info here',
-      },
-      host: 'localhost:3000',
-      schemes: ['http'],
+      // externalDocs: {
+      // url: 'https://swagger.io',
+      // description: 'Find more info here',
+      // },
+      host:
+        (process.env.HOST ?? 'localhost') + ':' + (process.env.PORT ?? '3000'),
+      schemes: ['http', 'https'],
       consumes: ['application/json'],
       produces: ['application/json'],
       tags: [
-        { name: 'user', description: 'User related end-points' },
-        { name: 'code', description: 'Code related end-points' },
-      ],
-      definitions: {
-        User: {
-          type: 'object',
-          required: ['id', 'email'],
-          properties: {
-            id: { type: 'string', format: 'uuid' },
-            firstName: { type: 'string' },
-            lastName: { type: 'string' },
-            email: { type: 'string', format: 'email' },
+        {
+          name: 'Game',
+          description: 'Game info from Steam',
+          externalDocs: {
+            url: 'https://developer.valvesoftware.com/wiki/Steam_Web_API',
           },
         },
+      ],
+      definitions: {
+        // User: {
+        //   type: 'object',
+        //   required: ['id', 'email'],
+        //   properties: {
+        //     id: { type: 'string', format: 'uuid' },
+        //     firstName: { type: 'string' },
+        //     lastName: { type: 'string' },
+        //     email: { type: 'string', format: 'email' },
+        //   },
+        // },
       },
       securityDefinitions: {
         apiKey: {
@@ -49,23 +55,8 @@ export default fp(async function (fastify: FastifyInstance) {
   fastify.register(fastifySwaggerUi, {
     routePrefix: '/docs',
     uiConfig: {
-      docExpansion: 'full',
+      docExpansion: 'none',
       deepLinking: false,
     },
-    uiHooks: {
-      onRequest: function (request, reply, next) {
-        console.log('onRequest');
-        next();
-      },
-      preHandler: function (request, reply, next) {
-        next();
-      },
-    },
-    staticCSP: true,
-    transformStaticCSP: (header) => header,
-    transformSpecification: (swaggerObject, request, reply) => {
-      return swaggerObject;
-    },
-    transformSpecificationClone: true,
   });
 });
